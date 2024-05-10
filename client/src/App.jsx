4d602ab041b5fe01781db233fa7e0e0aafafa438
pavanh <pavanhdsce@gmail.com>
2024-05-10 18:57:03 +0530

    tweaks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,34 +5,17 @@ import Appointment from "./component/Appointment/appointmentList.jsx";
 import Login from "./component/Login/login.jsx";
 import Register from "./component/Register/register.jsx";
 import { Route, Routes } from "react-router-dom"; // Import BrowserRouter, Route, and Routes
-const Doctor = () => {
-  return (
-    <>
-      <Appointment />
-      
-    </>
-  );
-};
-const Patient = () => {
-  return (
-    <>
-      <Appointment />
-    </>
-  );
-};
 
 function App() {
   return (
-    
-      <div style={{ padding: "20px" }}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-            <Route path="/doctor" element={<Doctor />} />
-            <Route path="/patient" element={<Patient />} />
-        </Routes>
-      </div>
-    
+    <div style={{ padding: "20px" }}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/doctor" element={<Appointment />} />
+        <Route path="/patient" element={<Appointment />} />
+      </Routes>
+    </div>
   );
 }
 
